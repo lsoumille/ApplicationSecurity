aa107e90fa7ce16b45380fdbd99cfaaf1343f7e0
lucas <lucas.soumille@etu.unice.fr>
2017-01-18 14:19:36 +0100

    First Commit

---
+++ 3DES/src/com/polytech/security/SqueletonTripleDES.java
@@ -0,0 +1,268 @@
+package com.polytech.security;
+
+
+
+import java.io.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.*;
+import java.util.*;
+
+public class SqueletonTripleDES{
+
+
+	static public void main(String[] argv){
+		
+		Provider prov = new org.bouncycastle.jce.provider.BouncyCastleProvider();
+		Security.addProvider(prov);
+		
+		try{
+	
+			if(argv.length>0){
+			
+				// Create a TripleDES object 
+				SqueletonTripleDES the3DES = new SqueletonTripleDES();
+			
+				if(argv[0].compareTo("-ECB")==0){
+					// EBC mode
+				  	// encrypt EBC mode
+				  	Vector Parameters= 
+					  	the3DES.encryptECB(
+					  			new FileInputStream(new File(argv[1])),  	// clear text file 
+				   	  			new FileOutputStream(new File(argv[2])), 	// file encrypted
+				   	  			"DES", 										// KeyGeneratorName
+				   	  			"DES/ECB/NoPadding"); 						// CipherName 
+				  	// decrypt EBC mode
+				  	the3DES.decryptECB(Parameters,				 			// the 3 DES keys
+				  				new FileInputStream(new File(argv[2])),  	// the encrypted file 
+				   	  			new FileOutputStream(new File(argv[3])),	// the decrypted file
+				   	  			"DES/ECB/NoPadding"); 		  				// CipherName
+				}	
+				else if(argv[0].compareTo("-CBC")==0){
+					// decryption
+				  	// encrypt CBC mode
+				  	Vector Parameters = 
+					  	the3DES.encryptCBC(
+					  			new FileInputStream(new File(argv[1])),  	// clear text file 
+				   	  			new FileOutputStream(new File(argv[2])), 	// file encrypted
+				   	  			"DES", 										// KeyGeneratorName
+					  			"DES/CBC/NoPadding"); 						// CipherName
+				   	  			//"DES/CBC/PKCS5Padding"); 					// CipherName 
+				  	// decrypt CBC mode	
+				  	the3DES.decryptCBC(
+				  				Parameters,				 					// the 3 DES keys
+			  					new FileInputStream(new File(argv[2])),  	// the encrypted file 
+			  					new FileOutputStream(new File(argv[3])),	// the decrypted file
+				  				"DES/CBC/NoPadding"); 						// CipherName			
+				  				//"DES/CBC/PKCS5Padding"); 		  			// CipherName	  
+				}
+			
+			}
+			
+			else{
+				System.out.println("java TripleDES -EBC clearTextFile EncryptedFile DecryptedFile");
+				System.out.println("java TripleDES -CBC clearTextFile EncryptedFile DecryptedFile");
+			} 
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("java TripleDES -EBC clearTextFile EncryptedFile DecryptedFile");
+			System.out.println("java TripleDES -CBC clearTextFile EncryptedFile DecryptedFile");
+		}
+	}
+
+	
+	/**
+	 * 3DES ECB Encryption
+	 */
+	private Vector encryptECB(FileInputStream in, 
+							FileOutputStream out, 
+							String KeyGeneratorInstanceName, 
+							String CipherInstanceName){
+		try{
+		
+			// GENERATE 3 DES KEYS
+			//Get a key Generator for DES Algorithm
+			KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyGeneratorInstanceName);
+            //K1
+            Key K1 = keyGenerator.generateKey();
+			//K2
+			Key K2 = keyGenerator.generateKey();
+			//K3
+			Key K3 = keyGenerator.generateKey();
+
+
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR ENCRYPTION 
+				// WITH THE FIRST GENERATED DES KEY
+            Cipher C1 = Cipher.getInstance(CipherInstanceName);
+            C1.init(Cipher.ENCRYPT_MODE, K1);
+			
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION
+				// WITH THE SECOND GENERATED DES KEY
+            Cipher C2 = Cipher.getInstance(CipherInstanceName);
+            C2.init(Cipher.DECRYPT_MODE, K2);
+				
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName 
+				// FOR ENCRYPTION
+				// WITH THE THIRD GENERATED DES KEY
+			Cipher C3 = Cipher.getInstance(CipherInstanceName);
+			C3.init(Cipher.ENCRYPT_MODE, K3);
+
+			// GET THE MESSAGE TO BE ENCRYPTED FROM IN
+            ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
+            int content;
+            while ((content = in.read()) != -1) {
+                dataStream.write(content);
+            }
+            byte[] message = dataStream.toByteArray();
+
+			// CIPHERING     
+				// CIPHER WITH THE FIRST KEY
+				// DECIPHER WITH THE SECOND KEY
+				// CIPHER WITH THE THIRD KEY
+				// write encrypted file
+            //byte[] encrypted = ;
+			// WRITE THE ENCRYPTED DATA IN OUT
+			
+			// return the DES keys list generated		
+			return null;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	/**
+	 * 3DES ECB Decryption 
+	 */
+	private void decryptECB(Vector Parameters, 
+						FileInputStream in, 
+						FileOutputStream out, 
+						String CipherInstanceName){
+		try{
+
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION 
+				// WITH THE THIRD GENERATED DES KEY
+			
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION
+				// WITH THE SECOND GENERATED DES KEY
+				
+			// CREATE A DES CIPHER OBJECT WITH DES/EBC/PKCS5PADDING FOR ENCRYPTION
+				// WITH CipherInstanceName
+				// FOR ENCRYPTION
+				// WITH THE FIRST GENERATED DES KEY
+			
+			// GET THE ENCRYPTED DATA FROM IN
+			
+			// DECIPHERING     
+				// DECIPHER WITH THE THIRD KEY
+				// 	CIPHER WITH THE SECOND KEY
+				// 	DECIPHER WITH THE FIRST KEY
+
+			// WRITE THE DECRYPTED DATA IN OUT
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+	  
+	/**
+	 * 3DES ECB Encryption
+	 */
+	private Vector encryptCBC(FileInputStream in, 
+							FileOutputStream out, 
+							String KeyGeneratorInstanceName, 
+							String CipherInstanceName){
+		try{
+		
+			// GENERATE 3 DES KEYS
+			// GENERATE THE IV
+		
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR ENCRYPTION 
+				// WITH THE FIRST GENERATED DES KEY
+			
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION
+				// WITH THE SECOND GENERATED DES KEY
+				
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName 
+				// FOR ENCRYPTION
+				// WITH THE THIRD GENERATED DES KEY
+				
+			// GET THE DATA TO BE ENCRYPTED FROM IN 
+			
+			// CIPHERING     
+				// CIPHER WITH THE FIRST KEY
+				// DECIPHER WITH THE SECOND KEY
+				// CIPHER WITH THE THIRD KEY
+
+			// WRITE THE ENCRYPTED DATA IN OUT
+			
+			// return the DES keys list generated		
+			return null;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * 3DES ECB Decryption 
+	 */
+	private void decryptCBC(Vector Parameters, 
+						FileInputStream in, 
+						FileOutputStream out, 
+						String CipherInstanceName){
+		try{
+		
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION 
+				// WITH THE THIRD GENERATED DES KEY
+			
+			// CREATE A DES CIPHER OBJECT 
+				// WITH CipherInstanceName
+				// FOR DECRYPTION
+				// WITH THE SECOND GENERATED DES KEY
+				
+			// CREATE A DES CIPHER OBJECT WITH DES/EBC/PKCS5PADDING FOR ENCRYPTION
+				// WITH CipherInstanceName
+				// FOR ENCRYPTION
+				// WITH THE FIRST GENERATED DES KEY
+			
+			// GET ENCRYPTED DATA FROM IN
+			
+			// DECIPHERING     
+				// DECIPHER WITH THE THIRD KEY
+				// 	CIPHER WITH THE SECOND KEY
+				// 	DECIPHER WITH THE FIRST KEY
+
+			// WRITE THE DECRYPTED DATA IN OUT
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+	}
+	  
+
+}
